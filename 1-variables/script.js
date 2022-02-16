909be001ef51192f7f7f83cae344ae621b51e068
hack <hugolgsuarez@gmail.com>
2022-02-16 09:01:27 +0100

    v_1

---
+++ 1-variables/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*VERSIÓN_1*/
+
+const name = "Hugo";
+const favColor = "amarelo";
+const age = 36;
+
+console.log(
+  `Ola. O meu nome é ${name}.Teño ${age} anos, e a miña cor favorita é o ${favColor}. `
+);
+
+/*VERSIÓN_2*/
+
+// const name = prompt("Bo día. Antes de continuar, preciso saber o seu nome");
+
+// const favColor = prompt(
+//   `Encantado de coñecel@ ${name}. Pódeme dicir cal é a súa cor favorita? `
+// );
+
+// const age = prompt(
+//   `Magnifica cor o ${favColor}. E para rematar, precisamos saber a súa idade. `
+// );
+
+// console.log(
+//   `Xa temos o que precisamos ${name}.En breves recibirá unha camisola ${favColor}. Que desexos ten para os ${
+//     parseInt(age) + 1
+//   } ? `
+// );
